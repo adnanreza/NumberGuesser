53a04ae93e27c4cee5030f532e8167bbecf7f8fd
Adnan Reza <adnan.reza@outlook.com>
2019-07-18 11:54:38 -0700

    Implement losing case

--- app.js
+++ app.js
@@ -35,12 +35,41 @@ UIguessBtn.addEventListener("click", function() {
   }
   //Check if won
   if (guess === winningNum) {
-    //disable input
-    UIguessInput.disabled = true;
-    UIguessInput.style.borderColor = "green";
-    //set message
-    setMessage(`${winningNum} is correct!, YOU JUST WON!`, "green");
+    //game over -won!
+    // //disable input
+    // UIguessInput.disabled = true;
+    // UIguessInput.style.borderColor = "green";
+    // //set message
+    // setMessage(`${winningNum} is correct!, YOU JUST WON!`, "green";
+    gameOver(true, `${winningNum} is correct!, YOU JUST WON!`, "green");
   } else {
+    //wrong mumber
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      //   //Game over - lost!
+      //   UIguessInput.disabled = true;
+      //   UIguessInput.style.borderColor = "red";
+      //   //set message
+      //   setMessage(
+      //     `Game over, you lost! The correct number was ${winningNum}`,
+      //     "red"
+      //   );
+      gameOver(
+        false,
+        `Game over, you lost! The correct number was ${winningNum}`
+      );
+    } else {
+      //Game continues - answer wrong
+      //change border color
+      UIguessInput.style.borderColor = "red";
+      //Clear input
+      UIguessInput.value = "";
+      //Notify user that this is wrong number
+      setMessage(
+        `${guess} is incorrect; You have ${guessesLeft} guesses left`,
+        "red"
+      );
+    }
   }
 });
 
@@ -48,4 +77,17 @@ UIguessBtn.addEventListener("click", function() {
 function setMessage(msg, color) {
   UImessage.style.color = color;
   UImessage.textContent = msg;
+}
+
+// gaveOver function
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : "red";
+  //disable input
+  UIguessInput.disabled = true;
+  UIguessInput.style.borderColor = color;
+  //set text color
+  UImessage.style.color = color;
+  //set message
+  setMessage(msg);
 }
